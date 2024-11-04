0dac79402df3611e99c801f890ca59dd5c6b8aa2
Eggzivine <138194896+Eggzivine@users.noreply.github.com>
2024-11-04 14:23:41 +0900

    Update 4_3_statistics_ans.cs

--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -15,13 +15,76 @@ namespace statistics
                 {"1004", "David", "94", "76", "92"},
                 {"1005", "Eve", "72", "95", "89"}
             };
-            // You can convert string to double by
-            // double.Parse(str)
 
             int stdCount = data.GetLength(0) - 1;
-            // ---------- TODO ----------
-            
-            // --------------------
+
+            double totalMath = 0, totalScience = 0, totalEnglish = 0;
+            double[] mathScores = new double[stdCount];
+            double[] scienceScores = new double[stdCount];
+            double[] englishScores = new double[stdCount];
+
+            for (int i = 1; i <= stdCount; i++)
+            {
+                double mathScore = double.Parse(data[i, 2]);
+                double scienceScore = double.Parse(data[i, 3]);
+                double englishScore = double.Parse(data[i, 4]);
+
+                totalMath += mathScore;
+                totalScience += scienceScore;
+                totalEnglish += englishScore;
+
+                mathScores[i - 1] = mathScore;
+                scienceScores[i - 1] = scienceScore;
+                englishScores[i - 1] = englishScore;
+            }
+
+            double avgMath = totalMath / stdCount;
+            double avgScience = totalScience / stdCount;
+            double avgEnglish = totalEnglish / stdCount;
+
+            double maxMath = mathScores[0], minMath = mathScores[0];
+            double maxScience = scienceScores[0], minScience = scienceScores[0];
+            double maxEnglish = englishScores[0], minEnglish = englishScores[0];
+
+            for (int i = 1; i < stdCount; i++)
+            {
+                if (mathScores[i] > maxMath) maxMath = mathScores[i];
+                if (mathScores[i] < minMath) minMath = mathScores[i];
+
+                if (scienceScores[i] > maxScience) maxScience = scienceScores[i];
+                if (scienceScores[i] < minScience) minScience = scienceScores[i];
+
+                if (englishScores[i] > maxEnglish) maxEnglish = englishScores[i];
+                if (englishScores[i] < minEnglish) minEnglish = englishScores[i];
+            }
+
+            Console.WriteLine("Average Scores: ");
+            Console.WriteLine($"Math: {avgMath:F2}");
+            Console.WriteLine($"Science: {avgScience:F2}");
+            Console.WriteLine($"English: {avgEnglish:F2}");
+            Console.WriteLine();
+
+            Console.WriteLine("Max and min Scores: ");
+            Console.WriteLine($"Math: ({maxMath}, {minMath})");
+            Console.WriteLine($"Science: ({maxScience}, {minScience})");
+            Console.WriteLine($"English: ({maxEnglish}, {minEnglish})");
+            Console.WriteLine();
+
+            var studentScores = new (string Name, double TotalScore)[stdCount];
+
+            for (int i = 1; i <= stdCount; i++)
+            {
+                double totalScore = mathScores[i - 1] + scienceScores[i - 1] + englishScores[i - 1];
+                studentScores[i - 1] = (data[i, 1], totalScore);
+            }
+
+            var rankedStudents = studentScores.OrderByDescending(s => s.TotalScore).ToArray();
+
+            Console.WriteLine("Students rank by total scores:");
+            for (int i = 0; i < rankedStudents.Length; i++)
+            {
+                Console.WriteLine($"{rankedStudents[i].Name}: {i + 1}th");
+            }
         }
     }
 }
